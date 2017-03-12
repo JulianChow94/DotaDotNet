f2446b92df874f5d716e12c361cd6b26f1fd32d6
JulianChow94 <JulianChow@outlook.com>
2017-03-11 20:48:17 -0500

    fixed up namespace mismatches

--- DotaApiCore/Authentication/IWebApiAuthenticator.cs
+++ DotaApiCore/Authentication/IWebApiAuthenticator.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DotaApiCore
+﻿namespace DotaApiCore.Authentication
 {
     public interface IWebApiAuthenticator
     {
