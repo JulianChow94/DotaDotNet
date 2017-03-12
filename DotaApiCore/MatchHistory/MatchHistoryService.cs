61c116c253599f55dc48e808630c05570fb45c98
Julian Chow <julianchow@outlook.com>
2017-03-11 20:26:54 -0500

    Update MatchHistoryService.cs

--- DotaApiCore/MatchHistory/MatchHistoryService.cs
+++ DotaApiCore/MatchHistory/MatchHistoryService.cs
@@ -17,8 +17,6 @@ namespace DotaApiCore.MatchHistory
             var url = new RequestBuilder().BuildLatestUserMatchDetailRequest(accountId, _apiKey);
             HttpClient client = new HttpClient();
 
-            //var result = client.GetAsync("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=E42BD4E39D900957FE9BDC8706798DA3&account_id=76561197992854119&matches_requested=1").Result;
-
             var result = client.GetAsync(url).Result;
 
             result.EnsureSuccessStatusCode();
