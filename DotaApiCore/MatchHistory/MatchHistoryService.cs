dce88638d61337975e90886046c920213b1eccaf
JulianChow94 <JulianChow@outlook.com>
2017-03-18 16:24:56 -0400

    HTTP requested routed through a request object

---
+++ DotaApiCore/MatchHistory/MatchHistoryService.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using DotaApiCore.MatchHistory.Models;
+using DotaApiCore.MatchHistory.Requests;
+using Newtonsoft.Json;
+
+namespace DotaApiCore.MatchHistory
+{
+    public class MatchHistoryService : IMatchHistoryService
+    {
+        private readonly string _apiKey;
+
+        public MatchHistoryService(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        public MatchHistoryRequestResult GetMatchHistory(long? accountId = null, 
+            int? heroId = null, int? gameMode = null, int? skill = null,
+            int? minPlayers = null, long? startingMatchId = null, int? matchesRequested = 100)
+        {
+            var matchHistoryRequest = new MatchHistoryRequest(_apiKey, accountId, heroId, gameMode, 
+                skill, minPlayers, startingMatchId, matchesRequested);
+
+            var responseBody = SendAndValidateRequest(matchHistoryRequest);
+
+            var results = JsonConvert.DeserializeObject<MatchHistoryRequestResult>(responseBody.Result);
+
+            return results;
+        }
+
+        private static async Task<string> SendAndValidateRequest(Request matchHistoryRequest)
+        {
+            var result = matchHistoryRequest.SendRequest();
+            result.EnsureSuccessStatusCode();
+
+            var responseBody = await result.Content.ReadAsStringAsync();
+
+            return responseBody;
+        }
+    }
+}
+    
---
+++ DotaApiCore/MatchHistory/Requests/MatchHistoryRequest.cs
@@ -0,0 +1,107 @@
+﻿using System.IO;
+using System.Net.Http;
+using DotaApiCore.MatchHistory.Models;
+using Newtonsoft.Json.Linq;
+
+namespace DotaApiCore.MatchHistory.Requests
+{
+    // https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=E42BD4E39D900957FE9BDC8706798DA3&account_id=76561197992854119&matches_requested=1
+    public class MatchHistoryRequest : Request
+    {
+        /* NOTE: LeagueID to be implemented as part of the "League sprint"
+            Omitting for now
+            Same situation for Tourney games only filter
+        */
+
+        private string MatchHistoryBaseUrl { get; set; }
+
+        public long? AccountId { get; set; }
+
+        //TODO: Enumerize
+        public int? HeroId { get; set; }
+
+        //TODO: Enumerize
+        public int? GameMode { get; set; }
+
+        //TODO: Enumerize
+        public int? Skill { get; set; }
+
+        public int? MinimumPlayers { get; set; }
+
+        public long? StartAtMatchId { get; set; }
+
+        public int? MatchesRequested { get; set; }
+
+        public MatchHistoryRequest(string apiKey, long? accountId = null, int? heroId = null, int? gameMode = null, int? skill = null, 
+            int? minPlayers = null, long? startingMatchId = null, int? matchesRequested = 100)
+        {
+            ApiKey = apiKey;
+            AccountId = accountId;
+            HeroId = heroId;
+            GameMode = gameMode;
+            Skill = skill;
+            MinimumPlayers = minPlayers;
+            StartAtMatchId = startingMatchId;
+            MatchesRequested = matchesRequested;
+
+            var config = JObject.Parse(File.ReadAllText(@"C:\Projects\DotaApi\DotaApiCore\config.json"));
+            var urlConfig = config.ToObject<UrlConfiguration>();
+            MatchHistoryBaseUrl = InitializeUrl(urlConfig);
+        }
+
+        protected sealed override string InitializeUrl(UrlConfiguration urlConfig)
+        {
+           return urlConfig.BaseUrl + urlConfig.GetMatchHistoryExtension + string.Format("?key={0}", ApiKey);
+        }
+
+        public override HttpResponseMessage SendRequest()
+        {
+            var requestUrl = BuildUrlParameters(MatchHistoryBaseUrl);
+
+            var client = new HttpClient();
+            var result = client.GetAsync(requestUrl).Result;
+
+            return result;
+        }
+
+        private string BuildUrlParameters(string requestUrl)
+        {
+            if (AccountId != null)
+            {
+                requestUrl = requestUrl + string.Format("&account_Id={0}", AccountId);
+            }
+
+            if (HeroId != null)
+            {
+                requestUrl = requestUrl + string.Format("&hero_id={0}", HeroId);
+            }
+
+            if (GameMode != null)
+            {
+                requestUrl = requestUrl + string.Format("&game_mode={0}", GameMode);
+            }
+
+            if (Skill != null)
+            {
+                requestUrl = requestUrl + string.Format("&skill={0}", Skill);
+            }
+
+            if (MinimumPlayers != null)
+            {
+                requestUrl = requestUrl + string.Format("&min_players={0}", MinimumPlayers);
+            }
+
+            if (StartAtMatchId != null)
+            {
+                requestUrl = requestUrl + string.Format("&start_at_match_id={0}", StartAtMatchId);
+            }
+
+            if (MatchesRequested != null)
+            {
+                requestUrl = requestUrl + string.Format("&matches_requested={0}", MatchesRequested);
+            }
+
+            return requestUrl;
+        }
+    }
+}
