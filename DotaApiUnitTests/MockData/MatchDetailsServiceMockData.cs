c869c6ddba3a1d10c977c02e05f53542d1b1eae2
LiquidWater <bluemoon_redsun@hotmail.com>
2017-04-24 18:09:13 -0400

    More stuff done for MatchDetails unit tests

---
+++ DotaApiUnitTests/MockData/MatchDetailsServiceMockData.cs
@@ -0,0 +1,10 @@
+﻿namespace DotaApiUnitTests.MockData
+{
+    internal static class MatchDetailsServiceMockData
+    {
+        public static string Success1 = "test1";
+        public static string Success2 = "test2";
+        public static string Failure1 = "fail1";
+        public static string Failure2 = "fail2";
+    }
+}
--- DotaApiUnitTests/TestMatchDetailsService.cs
+++ DotaApiUnitTests/TestMatchDetailsService.cs
@@ -3,18 +3,53 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Reflection;
-using DotaApiCore.MatchHistory;
+using DotaApiCore.MatchDetails.Models;
+using DotaApiCore.MatchDetails;
 using DotaApiCore.Requests;
 using DotaApiCore.SharedLib;
 using DotaApiUnitTests.MockData;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
+
 namespace DotaApiUnitTests
 {
     //TODO: Write testcases for TestMatchDetailsService
+    [TestCategory("Test MatchDetailsService")]
     [TestClass()]
-    class TestMatchDetailsService
+    public class TestMatchDetailsService
     {
+        private const string MockApiKey = "12345";
+        private const int MockMatchId = 3154;
+        private Mock<IHttpHandler> mockHandler;
+
+        [TestInitialize]
+        public void Init()
+        {
+            mockHandler = new Mock<IHttpHandler>();
+        }
+
+        [TestCleanup]
+        public void Destroy(){}
+
+        [TestMethod]
+        public void GetMatchDetails_Pass()
+        {
+            mockHandler.Setup(s => s.SendRequest(It.IsAny<string>())).Returns(MatchDetailsServiceMockData.Success1);
+            IMatchDetailsService service = new MatchDetailsService(mockHandler.Object, MockApiKey);
+            MatchDetailsRequestResult result = service.GetMatchDetails(MockMatchId);
+
+
+        }
+
+        [TestMethod]
+        public void GetMatchDetails_Fail()
+        {
+            mockHandler.Setup(s => s.SendRequest(It.IsAny<string>())).Returns(MatchDetailsServiceMockData.Failure1);
+            IMatchDetailsService service = new MatchDetailsService(mockHandler.Object, MockApiKey);
+            MatchDetailsRequestResult result = service.GetMatchDetails(MockMatchId);
+
+
+        }
     }
 }
